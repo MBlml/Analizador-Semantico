b45bc0e66eb114382743aa7a22f1ad14353937e2
Tu Nombre <you@example.com>
2023-03-14 00:15:53 -0600

    try

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
-//
 //  main.cpp
 //  CompilerProject3
-//
 //  Created by PatrickDD on 2021/5/10.
-//
 
 #include "LexicalAnalyzer.h"
 #include "GrammarAnalyzer.h"
 #include "SemanticAnalyzer.h"
-
-
 
 int main(int argc, const char * argv[]) {
     LexicalAnalyzer lex("p2.txt");
